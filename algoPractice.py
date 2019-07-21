eb6258c7894026ebc82513c23c7842f041ec05fd
steven <szapata427@yahoo.com>
2019-07-21 17:12:36 -0400

    isDiagnoalMatrix

--- algoPractice.py
+++ algoPractice.py
@@ -8,3 +8,16 @@ def add(n):
 
 add(29)
 
+def isDiagonalMatrix(matrix):
+    rows = len(matrix)
+    
+    columns = rows
+    print(columns)
+    for x in matrix:
+        print(len(x))
+        print(x)
+        if len(x) != rows:
+            return False
+    
+    return True
+
