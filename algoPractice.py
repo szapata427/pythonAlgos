2d3eb6121b501bca079e0da0a80722b137cf8581
steven <szapata427@yahoo.com>
2019-07-21 18:28:15 -0400

    alarm clock problem

--- algoPractice.py
+++ algoPractice.py
@@ -29,86 +29,109 @@
 # isDiagonalMatrix(matrix)
 
 
-# def alarmClock(setTime, timeToSet):
-#     setTimeArray = setTime.split(':')
-#     setTimeHour = setTimeArray[0]
-#     setTimeMin = setTimeArray[1]
+def alarmClock(setTime, timeToSet):
+    setTimeArray = setTime.split(':')
+    setTimeHour = setTimeArray[0]
+    setTimeMin = setTimeArray[1]
 
-#     timeToSetArray = timeToSet.split(':')
-#     timeToSetHour = timeToSetArray[0]
-#     timeToSetMin = timeToSetArray[1]
+    timeToSetArray = timeToSet.split(':')
+    timeToSetHour = timeToSetArray[0]
+    timeToSetMin = timeToSetArray[1]
     
     
-#     maxhour = 23
-#     minhour = 0
+    maxhour = 23
+    minhour = 0
     
-#     maxminute = 59
-#     minminute = 0
-    
-    
-#     hourdifference = int(setTimeHour) - int(timeToSetHour)
-#     minDifference = int(setTimeMin) - int(timeToSetMin)
-    
-#     gouphour = False
-#     godownhour = False
-    
-#     goUpMin = False
-#     goDownMin = False
-    
-#     godowndifference = abs(hourdifference - maxhour)
-#     goupdifference  = abs(hourdifference - minhour)
-    
-#     downminDifference = abs(minDifference - maxminute)
-#     upminDifference  = abs(minDifference - minminute)
+    maxminute = 59
+    minminute = 0
     
     
-#     if godowndifference == goupdifference:
-#         same = True
+    hourdifference = int(setTimeHour) - int(timeToSetHour)
+    minDifference = int(setTimeMin) - int(timeToSetMin)
+    
+    gouphour = False
+    godownhour = False
+    
+    goUpMin = False
+    goDownMin = False
+    
+    godowndifference = abs(hourdifference - maxhour)
+    goupdifference  = abs(hourdifference - minhour)
+    
+    downminDifference = abs(minDifference - maxminute)
+    upminDifference  = abs(minDifference - minminute)
+    
+    
+    if godowndifference == goupdifference:
+        same = True
 
-#     if godowndifference < goupdifference:
-#         print(godowndifference)
-#         godownhour = True
-#     else:
-#         print(goupdifference)
-#         gouphour = True
-    
-
-#     if downminDifference == upminDifference:
-#         samemin = True
-
-#     if downminDifference < upminDifference:
-#         print(downminDifference)
-#         goDownMin = True
-#     else:
-#         print(upminDifference)
-#         goUpMin = True
+    if godowndifference < goupdifference:
+        print(godowndifference)
+        godownhour = True
+    else:
+        print(goupdifference)
+        gouphour = True
     
 
-#     print(godowndifference)
+    if downminDifference == upminDifference:
+        samemin = True
 
-#     alarmHour = 0
-#     alarmMin = 0
+    if downminDifference < upminDifference:
+        print(downminDifference)
+        goDownMin = True
+    else:
+        print(upminDifference)
+        goUpMin = True
     
-#     print(godownhour)
-#     if same == True:
-#         alarmHour = 0
-#     elif godownhour == True:
-#         alarmHour = godowndifference
-#     else: 
-#         alarmHour = goupdifference
+
+    print(godowndifference)
+
+    alarmHour = 0
+    alarmMin = 0
+    
+    print(godownhour)
+    if same == True:
+        alarmHour = 0
+    elif godownhour == True:
+        alarmHour = godowndifference
+    else: 
+        alarmHour = goupdifference
         
-#     if samemin == True:
-#         alarmMin = 0
-#     elif goDownMin == True:
-#         alarmMin = downminDifference
-#     else: 
-#         alarmMin = upminDifference
+    if samemin == True:
+        alarmMin = 0
+    elif goDownMin == True:
+        alarmMin = downminDifference
+    else: 
+        alarmMin = upminDifference
     
-#     result = alarmHour + alarmMin
+    result = alarmHour + alarmMin
     
-#     return result
+    return result
 
-# print(alarmClock("08:45", "08:00"))
+print(alarmClock("08:45", "08:00"))
+
+
+
+
+
+# def birthdayCakeCandles(ar):
+#     count = 0
+#     max = 0
+#     for x in ar:
+#         if x >= max:
+#             max = x
+
+#     for k in ar:
+#         if k == max:
+#             count += 1
+
+#     return count
+
+
+
+# candlesArray = [10,18,90,90,13,90,75,90,8,90,43]
+# # candlesArray = [4,3,2,1,3]
+# print(birthdayCakeCandles(candlesArray))
 
 
 # def nearestgreates(a):
@@ -149,25 +172,3 @@
 
 # a = [1, 4, 2, 1, 7, 6]
 # print(nearestgreates(a))
-
-
-
-
-def birthdayCakeCandles(ar):
-    count = 0
-    max = 0
-    for x in ar:
-        if x >= max:
-            max = x
-
-    for k in ar:
-        if k == max:
-            count += 1
-
-    return count
-
-
-
-candlesArray = [10,18,90,90,13,90,75,90,8,90,43]
-# candlesArray = [4,3,2,1,3]
-print(birthdayCakeCandles(candlesArray))
